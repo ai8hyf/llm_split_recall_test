6239e87eefc4648bb007a536508155797d4c6861
Yifei <ai8hyf@gmail.com>
2024-03-26 14:51:21 +0000

    Create Project

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # llm_split_recall_test
 A simple and efficient benchmark to evaluate in-context recall performance of Large Language Models (LLMs)
+
+## Task Description
+**Task Easy:** Sentence Split & Recall: for a given paragraph (the abstract section of the paper), split the paragraph into individual sentences
+**Task Hard:** Sentence Split & Recall with Long Context: for a given paragraph (the abstract section of the paper) in a long document (determined by `hard_task_max_char`), split the paragraph into individual sentences
+
+## Dataset
+In the `eval_data.json` file, there are 10 papers from ACL 2023. File structure looks like following:
+
+    [
+    		{
+    			"paper_id": int,
+    			"paper_title": "paper title",
+    			"paper_url": "link_to_the_paper",
+    			"abstract_sentences": ["sentence1", "sentence2", ...],
+    			"full_text": "full OCR text of the paper",
+    		},
+    		...
+    ]
+
+I have manually cleaned and splitted the abstracts into sentences.
+The abstract in the `full_text` is also replaced with the cleaned sentences.
+
+## Run the eval
+ 1. Clone the repo: `git clone https://github.com/ai8hyf/llm_split_recall_test`
+ 2. Install openai: `pip install openai`
+ 3. Provide your own openai api key or use your local LLM config. You can find the parameters inside `split_and_recall.py`. You can also choose the task (easy or hard) and context length for the hard task in the same file.
+ 4. Run the eval: `python split_and_recall.py`
+
+## Preliminary Results
+### Easy Task (hosted on vLLM, temp 0.1)
+```markdown
+| Model                    | Precision |
+|--------------------------|-----------|
+| Mistral 7B Instruct v0.2 |    61.04% |
+| Mixtral 8x7B Instruct    |    87.01% |
+| Qwen 1.5 72B Chat (4bit) |    96.10% |
+| GPT-3.5-Turbo            |    97.40% |
+| GPT-4-Turbo              |    98.70% |
+```
+### Hard Task (hosted on vLLM, temp 0.1)
+```markdown
+| Model                    | @ 2,500 Token | @ 5,000 Token | @ 8,000 Token |
+|--------------------------|---------------|---------------|---------------|
+| Mistral 7B Instruct v0.2 |         0.04% |            0% |            0% |
+| Mixtral 8x7B Instruct    |        83.12% |            0% |            0% |
+| Qwen 1.5 72B Chat (4bit) |        89.61% |        88.31% |        83.12% |
+```
+
+## Limitations
+The eval data may not be representative.
+Depending on the inference engine, prompt, and hyper-parameter settings, the benchmark scores may vary.
+
+## Contributing
+Please feel free to start new PRs!
+
+## Citation
+If you use Split&Recall in your research, please cite this project as:
+```
+@misc{splitNrecall,
+  author = {Yifei Hu},
+  title = {Split and Recall: A simple and efficient benchmark to evaluate in-context recall performance of Language Models},
+  year = {2024},
+  publisher = {GitHub},
+  journal = {GitHub repository},
+  howpublished = {\url{https://github.com/ai8hyf/llm_split_recall_test}},
+}
+```
---
+++ split_and_recall.py
@@ -0,0 +1,147 @@
+import json
+from openai import OpenAI
+
+eval_task = "easy" # or choose "hard" and provide the max_char parameter
+hard_task_max_char = 10000 # how many characters to provide to the model for the hard task
+
+oai_api_key = "your-oai-api-key" # if you want to use the openai api
+oai_model = "gpt-3.5-turbo" # if you want to use the openai api
+
+local_llm_api_base = "http://localhost:8080/v1" # if you want to use the local model
+local_llm_model = "path_to_your_local_model" # if you want to use the local model
+
+model_temperature = 0.2 # feel free to tweak this parameter
+
+# load eval_data.json from the same directory
+with open('eval_data.json', 'r') as f:
+	eval_data = json.load(f)
+	# eval data structure:
+	# [
+	# 	{
+	# 		"paper_id": int,
+	# 		"paper_title": "paper title",
+	# 		"paper_url": "link_to_the_paper",
+	# 		"abstract_sentences": ["sentence1", "sentence2", ...],
+	# 		"full_text": "full OCR text of the paper",
+	# 	},
+	# 	...
+	# ]
+	# Note: 
+	# 1. I have manually cleaned and splitted the abstracts into sentences. 
+	# 2. The abstract in the full_text is also replaced with the cleaned sentences.
+	# 3. Currently, there are 10 documents (papers) in the eval_data.json. They are from ACL 2023.
+
+def call_openai_api(prompt):
+	client = OpenAI(
+		# base_url=local_llm_api_base, # uncomment this line if you want to use the local model
+		api_key=oai_api_key, # change to api_key=local_llm_api_key if you want to use the local model
+	)
+	completion = client.chat.completions.create(
+		model= oai_model, # change to model=local_llm_model if you want to use the local model
+		messages=[
+			{"role": "system", "content": "You are a helpful assistant."}, # for mixtral models, you can comment out the system message
+			{"role": "user", "content": prompt},
+		],
+		# max_tokens = 500, # you can uncomment this line if you want to limit the tokens
+		temperature = model_temperature,
+	)
+	
+	return completion.choices[0].message.content
+
+def build_easy_task_instruction(sentences):
+	instruction = "Below is the abstract section from an academic paper.\n\n"
+
+	instruction += "### The abstract: "
+	for sentence in sentences:
+		instruction += sentence + " "
+	instruction += "\n\n"
+
+	instruction += "Your task is to split the abstract into sentences. You answer can only include the sentences. Split them with newline symbol. Start each sentence with # symble. Do not try to modify the sentences. Do not add anything before or after the sentences.\n\n"
+	instruction += "### Your answer: \n"
+
+	return instruction
+
+def build_hard_task_instruction(full_text, max_char):
+	instruction = "Below is a part of an OCR result of an academic paper.\n\n"
+
+	instruction += "### The OCR result: \n"
+	instruction += full_text[:max_char]
+	instruction += "\n### END OF OCR RESULT\n\n"
+
+	instruction += "Your task is to split the abstract section into sentences. You answer can only include the sentences. Split them with newline symbol. Start each sentence with # symble. Do not try to modify the sentences. Do not add anything before or after the sentences.\n\n"
+	instruction += "### Your answer: \n"
+
+	return instruction
+
+def evaluate_easy_task():
+	total_sentence_count = 0
+	total_correct_sentence_count = 0
+	total_document_count = len(eval_data)
+	processed_document_count = 0
+
+	print("=== Easy task eval starts ===")
+
+	for doc in eval_data:
+		instruction = build_easy_task_instruction(doc['abstract_sentences'])
+		response = call_openai_api(instruction)
+		generated_sentences = response.split("\n")
+
+		total_sentence_count += len(doc['abstract_sentences'])
+
+		document_correct_sentence_count = 0
+
+		processed_document_count += 1
+		
+		for sentence in generated_sentences:
+			sentence = sentence.strip()
+			if sentence.startswith("#"):
+				sentence = sentence[1:].strip()
+			if sentence in doc['abstract_sentences']:
+				total_correct_sentence_count += 1
+				document_correct_sentence_count += 1
+
+		print(f"Document: {doc['paper_url']}")
+		print("Success rate: ", str(document_correct_sentence_count) + "/" + str(len(doc['abstract_sentences'])))
+		print("=== " + str(processed_document_count) + "/" + str(total_document_count) + " documents processed ===")
+
+	print("=== Easy task eval result: ===")
+	print("Total success rate: ", str(total_correct_sentence_count) + "/" + str(total_sentence_count) + " = " + str(total_correct_sentence_count / total_sentence_count))
+
+def evaluate_hard_task():
+	total_sentence_count = 0
+	total_correct_sentence_count = 0
+	total_document_count = len(eval_data)
+	processed_document_count = 0
+
+	print("=== Hard task eval starts ===")
+
+	for doc in eval_data:
+		instruction = build_hard_task_instruction(doc['full_text'], hard_task_max_char)
+		response = call_openai_api(instruction)
+		generated_sentences = response.split("\n")
+
+		total_sentence_count += len(doc['abstract_sentences'])
+
+		document_correct_sentence_count = 0
+
+		processed_document_count += 1
+		
+		for sentence in generated_sentences:
+			sentence = sentence.strip()
+			if sentence.startswith("#"):
+				sentence = sentence[1:].strip()
+			if sentence in doc['abstract_sentences']:
+				total_correct_sentence_count += 1
+				document_correct_sentence_count += 1
+
+		print(f"Document: {doc['paper_url']}")
+		print("Success rate: ", str(document_correct_sentence_count) + "/" + str(len(doc['abstract_sentences'])))
+		print("=== " + str(processed_document_count) + "/" + str(total_document_count) + " documents processed ===")
+
+	print("=== Hard task eval result: ===")
+	print("Total success rate: ", str(total_correct_sentence_count) + "/" + str(total_sentence_count) + " = " + str(total_correct_sentence_count / total_sentence_count))
+
+if eval_task == "easy":
+	evaluate_easy_task()
+elif eval_task == "hard":
+	evaluate_hard_task()
